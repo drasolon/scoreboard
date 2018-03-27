9273715000697e116e3b4e11de753ff725f00318
osard <34164498+osard@users.noreply.github.com>
2018-03-27 16:55:49 +0200

    updated route and form validators

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const qs = require('querystring');
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
+http.createServer((req, res) => {
+  let q = url.parse(req.url, true);
+  let filename = "." + req.url;
+
+  if (req.method === 'POST') {
+    if (req.url === '/players.html') {
+      let body = [];
+
+      req.on('error', (err) => {
+        console.error(err);
+      }).on('data', (data) => {
+        body += data;
+      }).on('end', () => {
+        let formData = qs.parse(body);
+        console.log(formData);
+      });
+
+    }
+  };
+
+  fs.readFile(filename, (err, data) => {
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.writeHead(404, { 'Content-Type': 'text/html' });
       return res.end("404 Not Found");
-    }  
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     return res.end();
   });
