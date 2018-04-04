6571703950cfb84d0270fbabd8ff0e1a68fc8e11
osard <34164498+osard@users.noreply.github.com>
2018-04-04 12:45:29 +0200

    created pug files and updated app

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const pug = require('pug');
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(express.static('views'));
+/* app.use(express.static('scripts')); */
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.post('/players', (req, res) => {
+  res.render('players', {numberPlayers: req.body.inputNumberPlayers})
+});
+app.post('/initiateGame', (req, res) => {
   console.log(req.body);
-  res.render('players')
+  res.render('initiateGame', {playerName: req.body});
 });
 app.get('/header', function (req, res) {
   res.render('header');
