3214ed36d6e070d01f510cddc9dc5331196f5aad
osard <34164498+osard@users.noreply.github.com>
2018-07-23 15:44:40 +0200

    pagination

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,24 +1,53 @@
 const Game = require('../models/game');
 const tools = require('../private/tools');
 
+exports.getPagination = async (req, res, next) => {
+  const requestedPage = (req.params.page == null ? 1 : Number(req.params.page));
+  const gamesPerPage = 5;
+  const previousPage = (requestedPage === 1 ? 1 : (requestedPage - 1));
+
+  // If the user click on a pagination link, skip the previous games
+  const gamesToSkip = (requestedPage - 1) * gamesPerPage;
+  const promiseNumberOfGames = Game.count({ owner: req.session.id }).exec();
+  try {
+    // Calculate the amount of pagination page needed
+    const amountOfPages = Math.ceil(await promiseNumberOfGames / gamesPerPage);
+    const nextPage = (requestedPage === amountOfPages ? amountOfPages : (requestedPage + 1));
+    res.locals.previousPage = previousPage;
+    res.locals.nextPage = nextPage;
+    res.locals.amountOfPages = amountOfPages;
+    res.locals.gamesToSkip = gamesToSkip;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
+
 // Get games with the same session id as the user
-exports.getGames = (req, res, next) => {
+exports.getGames = async (req, res, next) => {
+  // Clear the list of game if user
   if (req.query.clear === 'true') {
     res.clearCookie('scoreboard');
     return res.render('index');
   }
 
-  Game.find({ owner: req.session.id }, (err, retrievedGames) => {
-    if (err) { return next(err); }
-    if (retrievedGames != null) {
-      // Format the dates
-      let temp;
-      retrievedGames.forEach((game) => {
-        temp = tools.formatDate(game.date);
-        game.dateDiff = temp;
-      });
-    }
-    return res.render('index', { games: retrievedGames });
-  });
+  const previousPage = res.locals.previousPage;
+  const nextPage = res.locals.nextPage;
+  const amountOfPages = res.locals.amountOfPages;
+  const gamesToSkip = res.locals.gamesToSkip;
+  const promiseRetrievedGames = Game.find({ owner: req.session.id }, null, { skip: gamesToSkip, limit: 5 }).exec();
+
+  try {
+    const retrievedGames = await promiseRetrievedGames;
+    let temp;
+    retrievedGames.forEach((game) => {
+      temp = tools.formatDate(game.date);
+      game.dateDiff = temp;
+    });
+
+    return res.render('index', { games: retrievedGames, amountOfPages, previousPage, nextPage });
+  } catch (err) {
+    return next(err);
+  }
 };
 
--- routes/root.js
+++ routes/root.js
@@ -5,7 +5,10 @@ const indexController = require('../controllers/indexController');
 const gameInstance = require('../models/gameInstance');
 
 // GET request to render index page
-router.get('/', indexController.getGames);
+router.get('/', indexController.getPagination, indexController.getGames);
+
+// GET request to render a specific page from the list of games
+router.get('/page/:page', indexController.getPagination, indexController.getGames);
 
 // GET request to render new game page
 router.get('/new', (req, res) => {
@@ -15,4 +18,5 @@ router.get('/new', (req, res) => {
 
 // GET request to rend the about page
 router.get('/about', (req, res) => res.render('about'));
+
 module.exports = router;
