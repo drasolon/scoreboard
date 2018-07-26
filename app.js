c86906038230c80a0420c20e6e46c7ac76a94cc0
osard <34164498+osard@users.noreply.github.com>
2018-07-26 16:33:40 +0200

    update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const mongoose = require('mongoose');
 const app = express();
 const mongoDB = config.db.url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
