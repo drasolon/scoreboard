3383ff1eecea56e28a0ba4e4d63a40a380949ab6
osard <34164498+osard@users.noreply.github.com>
2019-11-11 18:20:46 +0100

    changed env variables

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const path = require('path');
-const game = require('./routes/game');
-const root = require('./routes/root');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+const game = require("./routes/game");
+const root = require("./routes/root");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const config = require("./config.js");
 const app = express();
-const mongoDB = process.env.DB_URL;
-mongoose.connect(mongoDB, { useNewUrlParser: true }, (err) => {
-  if (err) { app.use((req, res, next) => next(err)); }
+const mongoDB = config.db.url;
+mongoose.connect(mongoDB, { useNewUrlParser: true }, err => {
+  if (err) {
+    app.use((req, res, next) => next(err));
+  }
 });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'pug')
+app
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "pug")
   .use(helmet())
   .use(express.static(`${__dirname}/public`))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
-  .use(session({
-    name: 'scoreboard',
-    secret: process.env.COOKIE_SECRET,
-    store: new MongoStore({ mongooseConnection: db }),
-    saveUninitialized: true,
-    resave: false,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      /* domain: 'example.com',
+  .use(
+    session({
+      name: "scoreboard",
+      secret: config.cookie.secret,
+      store: new MongoStore({ mongooseConnection: db }),
+      saveUninitialized: true,
+      resave: false,
+      cookie: {
+        secure: false,
+        httpOnly: true,
+        /* domain: 'example.com',
       path: '/index', */
-      expires: new Date(Date.now() + (365 * 24 * 3600 * 1000)) // 365 days
-    }
-  }))
-  .use('/game', game)
-  .use('/', root)
+        expires: new Date(Date.now() + 365 * 24 * 3600 * 1000) // 365 days
+      }
+    })
+  )
+  .use("/game", game)
+  .use("/", root)
 
   // Catch 404 and forward to error handler
   .use((req, res, next) => {
-    const err = new Error('Page Not Found');
+    const err = new Error("Page Not Found");
     err.status = 404;
     return next(err);
   })
@@ -50,12 +56,11 @@ app.set('views', path.join(__dirname, 'views'))
     // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.status = err.status;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
     // Render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
   });
-const server = app.listen(8080, () => {
-});
+const server = app.listen(8080, () => {});
 
 module.exports = server;
